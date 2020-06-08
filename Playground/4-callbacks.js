d95f47d8032168ece11fa4ea83a6245c8959dc3d
yohad <jadhad@gmail.com>
2020-06-08 22:11:37 +0300

    Promises code (Section 10 lesson 79)

--- Playground/4-callbacks.js
+++ Playground/4-callbacks.js
@@ -1,33 +1,50 @@
-setTimeout(() => {
-    console.log('two seconds are up')
-}, 2000)
+// setTimeout(() => {
+//     console.log('two seconds are up')
+// }, 2000)
+//
+// const names = ['AAA', 'BBB', 'CCC']
+// const shortNames = names.filter((name) => {
+//     return name.length <= 4
+// })
+//
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+//
+//         callback(data)
+//     }, 2000)
+// }
+//
+// geocode('Address', (data) => {
+//     console.log(data)
+// })
+//
+// const add = (num1, num2, callback) => {
+//     setTimeout(() => {
+//         callback(num1+num2)
+//     }, 2000)
+// }
+//
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
 
-const names = ['AAA', 'BBB', 'CCC']
-const shortNames = names.filter((name) => {
-    return name.length <= 4
-})
 
-const geocode = (address, callback) => {
+// callback example
+const doWorkCallback = (callback) => {
     setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
-
-        callback(data)
+        //callback('This is my error!', undefined)
+        callback(undefined, [1, 4, 7])
     }, 2000)
 }
 
-geocode('Address', (data) => {
-    console.log(data)
-})
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
 
-const add = (num1, num2, callback) => {
-    setTimeout(() => {
-        callback(num1+num2)
-    }, 2000)
-}
-
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
+    console.log(result)
 })
